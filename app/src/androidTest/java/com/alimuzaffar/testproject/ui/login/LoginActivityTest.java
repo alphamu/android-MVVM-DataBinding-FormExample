c0eb8f31b47be86b69604662f5a6015f21fcd9c9
Ali Muzaffar <ali.muzaffar@bcgdv.com>
2018-08-27 17:37:55 +1000

    Update the instrumentation test manually to make it cleaner and more readable

--- app/src/androidTest/java/com/alimuzaffar/testproject/ui/login/LoginActivityTest.java
+++ app/src/androidTest/java/com/alimuzaffar/testproject/ui/login/LoginActivityTest.java
@@ -1,20 +1,12 @@
 package com.alimuzaffar.testproject.ui.login;
 
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
 import com.alimuzaffar.testproject.R;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
@@ -23,10 +15,9 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -37,54 +28,10 @@ public class LoginActivityTest {
 
     @Test
     public void loginActivityTest() {
-
-        ViewInteraction appCompatEditText11 = onView(
-                allOf(withId(R.id.txtEmail),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()));
-        appCompatEditText11.perform(replaceText("asd@asd.cc"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText12 = onView(
-                allOf(withId(R.id.txtPassword),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                3),
-                        isDisplayed()));
-        appCompatEditText12.perform(replaceText("helloq"), closeSoftKeyboard());
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.button), withText("Login"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                4),
-                        isDisplayed()));
-        appCompatButton.perform(click());
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+        // Click is needed since we perform validation on lose focus
+        onView(withId(R.id.txtEmail)).perform(click(), replaceText("asd@asd.com"));
+        onView(withId(R.id.txtPassword)).perform(click(), replaceText("Password"), closeSoftKeyboard());
+        onView(withId(R.id.button)).check(matches(isEnabled()));
+        onView(withId(R.id.button)).perform(click());
     }
 }
